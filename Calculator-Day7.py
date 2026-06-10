e555a80f77001c7d92867e7b657324309adfed90
piousafutu-bit <piousafutu@gmail.com>
2026-06-10 07:43:37 +0000

    Arithmetic operations to calculator

---
+++ Calculator-Day7.py
@@ -0,0 +1,8 @@
+print("Day 7: Smart Calculator")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
